8f1cf19aa4da2739dc22f380ed9c24a2746334fe
shaikh-kashish-bca <shaikhkashish183@gmail.com>
2026-07-10 00:21:35 +0530

    04_Multiplecation_Of_Two_Number.cpp

---
+++ 04_Multiplecation_Of_Two_Number.cpp
@@ -0,0 +1,21 @@
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2, sub;
+    
+    cout << "Enter first number: ";
+    cin >> num1;
+    
+    cout << "Enter second number: ";
+    cin >> num2;
+    
+    sub = num1 - num2;
+    
+    cout << "Subtraction = " << sub;
+    
+    return 0;
+}
