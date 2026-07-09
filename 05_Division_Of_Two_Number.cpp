ea0c2bc6264fc6153d60c7f1fec95944ed2593d0
shaikh-kashish-bca <shaikhkashish183@gmail.com>
2026-07-10 00:23:03 +0530

    05_Division_Of_Two_Number.cpp

---
+++ 05_Division_Of_Two_Number.cpp
@@ -0,0 +1,21 @@
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float num1, num2, div;
+    
+    cout << "Enter first number: ";
+    cin >> num1;
+    
+    cout << "Enter second number: ";
+    cin >> num2;
+    
+    div = num1 / num2;
+    
+    cout << "Division = " << div;
+    
+    return 0;
+}
